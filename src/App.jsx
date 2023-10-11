66143f0382e5ae30404bec674a6ffb4a85761501
Your Name <you@example.com>
2023-10-11 15:46:07 +0600

    some design polish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-300">
-        Hello world!
-      </h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/Pages/Home/Home/HomePage.jsx
+++ src/Pages/Home/Home/HomePage.jsx
@@ -1,13 +1,11 @@
 import Advantages from "../HomeComponents/Advantages";
 import Banner from "../HomeComponents/Banner/Banner";
-// import ContactForm from "../HomeComponents/Contact/ContactForm";
 import Curved from "../HomeComponents/Curved/Curved";
 import Discount from "../HomeComponents/Discount/Discount";
 import EasyPlanner from "../HomeComponents/EasyTripPlanner/EasyPlanner";
 import FindTour from "../HomeComponents/FindTour/FindTour";
 import PlaceSearch from "../HomeComponents/PlaceSearch";
 import Places from "../HomeComponents/Places/Places";
-// import Tending from "../HomeComponents/TrandingPlace/Tending";
 import WorldOverview from "../HomeComponents/WorldOverview/WorldOverview";
 import Counted from "../../../Components/Counted";
 import Testimonial from "../HomeComponents/Testimonial/Testimonial";
